005faa0173e31008236ced4f4fb4369aa0de7ff2
Workpluss2024 <workpluss.app@gmail.com>
2024-09-24 19:34:56 +0200

    Updated the work post model

--- server/models/Work.js
+++ server/models/Work.js
@@ -5,25 +5,52 @@ const workSchema = new Schema({
   uuid: {
     type: String,
     default: uuidv4,
-    unique: true
+    unique: true,
   },
-  workTitle: String,
-  workSubTitle: String,
-  workDescription: String,
-  averagePayPerHour: Number,
-  status: String,
-  fieldsWorked: [String], // Array of fields or industries the user has worked in
-  successfulJobsCount: Number, // Total number of jobs successfully completed by the user
-  applications: [{
-    applicantId: { type: Schema.Types.ObjectId, ref: 'User' },
-    applicationDate: Date,
-    status: String,
-  }],
-  feedback: [{
-    fromUserId: { type: Schema.Types.ObjectId, ref: 'User' },
-    content: String,
-    date: Date,
-  }],
+  jobTitle: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  postalAddress: {
+    type: String,
+    required: true,
+  },
+  area: {
+    type: String,
+    required: true,
+  },
+  postalCode: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  startingTime: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,  
+    required: true,
+  },
+  maxPerHourAmount: {
+    type: Number,
+    required: true,
+  },
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
 });
 
 export default model('Work', workSchema);
